d0d62f0dffb1dcb9ae50f6cdc99ee47ef37eb861
Jacob Knowlton <jacob@hexagonitsolutions.com>
2023-12-15 16:09:01 -0700

    Changed initial script to only install base on first run

--- 17.0/run-odoo.sh
+++ 17.0/run-odoo.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-/odoo-dev/odoo/odoo-bin --addons-path=/odoo-dev/odoo/addons,/odoo-dev/hits-apps --db_host db --db_port 5432 --db_user odoo --db_password odoo -d odoo -i base
+
+CMD="/odoo-dev/odoo/odoo-bin --addons-path=/odoo-dev/odoo/addons,/odoo-dev/enterprise,/odoo-dev/hits-apps --db_host db --db_port 5432 --db_user odoo --db_password odoo -d odoo"
+
+if ! [ -f /odoo-dev/.ran_odoo ]
+then
+    touch /odoo-dev/.ran_odoo
+    CMD += " -i base"
+fi
+
+$CMD
